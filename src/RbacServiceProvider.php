94a1cd15f0b279797b6862e55f36ffdc11393fc9
wangheng19851019@126.com <wangheng19851019@126.com>
2018-10-11 14:26:46 +0800

    update

--- src/RbacServiceProvider.php
+++ src/RbacServiceProvider.php
@@ -10,12 +10,12 @@ namespace w337886915\rbac;
 use Illuminate\Support\ServiceProvider;
 class RbacServiceProvider extends ServiceProvider
 {
+
     public function boot(){
         //配置路由
         $this->loadRoutesFrom(__DIR__.'/routers/routes.php');
     }
 
-    public function register(){
-        $this->registerRouteMiddleware();
-    }
+
+
 }
